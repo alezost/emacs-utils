09ba99f725cd05df0e7472ddc38da0020bcb751c
Alex Kost <alezost@gmail.com>
2014-01-29 11:43:27 +0400

    Add function for downloading URL with wget

--- utl-w3m.el
+++ utl-w3m.el
@@ -23,6 +23,27 @@
         (w3m-goto-url w3m-previous-url))
     (message "No 'previous' link found.")))
 
+
+;;; Download with wget
+
+(defvar utl-w3m-download-dir nil
+  "Default directory for downloading with wget.")
+
+;; from <http://www.emacswiki.org/emacs/WThreeMHintsAndTips>
+(defun utl-w3m-download-with-wget (dir)
+  "Download url at point to the directory DIR with wget."
+  (interactive
+   (list (ido-read-directory-name "Save to: " utl-w3m-download-dir)))
+  (let ((url (or (w3m-anchor) (w3m-image))))
+    (if url
+	(let ((proc (start-process "wget" (format "*wget %s*" url)
+				   "wget" "--passive-ftp" "-nv"
+				   "-P" (expand-file-name dir) url)))
+	  (set-process-sentinel proc
+                                (lambda (proc str)
+                                  (message "Download finished."))))
+      (message "Nothing to get."))))
+
 (provide 'utl-w3m)
 
 ;;; utl-w3m.el ends here
