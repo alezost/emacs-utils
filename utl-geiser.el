f474d23f3132c0c0bc52ce75052f749d9dca6d0b
Alex Kost <alezost@gmail.com>
2014-04-26 10:10:18 +0400

    Add 2 functions for geiser-repl

--- utl-geiser.el
+++ utl-geiser.el
@@ -1,7 +1,7 @@
 ;;; utl-geiser.el --- Additional functionality for geiser
 
 ;; Author: Alex Kost <alezost@gmail.com>
-;; Created: 10 Sep 2013
+;; Created: 13 Apr 2014
 
 ;;; Code:
 
@@ -14,6 +14,23 @@ ARG is passed to `geiser-eval-last-sexp'."
   (if (use-region-p)
       (geiser-eval-region (region-beginning) (region-end))
     (geiser-eval-last-sexp arg)))
+
+(defun utl-geiser-repl-enter-dwim ()
+  "Send input or goto the error at point.
+Substitution for `geiser-repl--maybe-send'."
+  (interactive)
+  (cond ((< (point) (geiser-repl--last-prompt-start))
+         (if (geiser-repl--is-history-input)
+             (geiser-repl--grab-input)
+           (ignore-errors (compile-goto-error))))
+        (t
+         (geiser-repl--send-input))))
+
+(defun utl-geiser-repl-kill-whole-line (arg)
+  "Similar to `kill-whole-line', but respect geiser repl prompt."
+  (interactive "p")
+  (kill-region (comint-line-beginning-position)
+               (progn (forward-line arg) (point))))
 
 (provide 'utl-geiser)
 
