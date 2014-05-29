b8f39234173d34b44b73c103f12a4ebd3323276e
Alex Kost <alezost@gmail.com>
2014-05-29 17:08:38 +0400

    Improve `utl-quelpa-update-install' function

--- utl-elpa.el
+++ utl-elpa.el
@@ -10,41 +10,46 @@
 Can be a package name (symbol) or a full quelpa recipe (list).")
 
 ;;;###autoload
-(defun utl-quelpa-update-install (&rest packages)
+(defun utl-quelpa-update-install (&optional packages)
   "Update quelpa PACKAGES.
-Install quelpa if neeeded.
 
-Each element from PACKAGES should be a name of the package from
-`utl-quelpa-recipes'.  If PACKAGES is nil, install all packages
+If quelpa is not installed, install it and all of the packages
 from `utl-quelpa-recipes'.
+
+PACKAGES is a list of names of the packages from
+`utl-quelpa-recipes'.  If PACKAGES is nil, do not update/install
+any package (except the above case).  If PACKAGES is t,
+update/install all packages.
 
 Interactively, prompt for a package to update/install.  With
 prefix, update all packages."
   (interactive
-   (unless current-prefix-arg
-     (list
+   (list
+    (if current-prefix-arg
+        t
       (let ((names (mapcar (lambda (recipe)
                              (symbol-name
                               (if (listp recipe) (car recipe) recipe)))
                            utl-quelpa-recipes)))
-        (intern (completing-read "Update/install: " names nil t))))))
-  (unless (require 'quelpa nil t)
+        (list (intern (completing-read "Update/install: " names nil t)))))))
+  (if (require 'quelpa nil t)
+      (if (eq packages t)
+          (dolist (recipe utl-quelpa-recipes)
+            (quelpa recipe))
+        (dolist (name packages)
+          (let ((recipe (car (cl-member-if
+                              (lambda (recipe)
+                                (eq name
+                                    (if (listp recipe) (car recipe) recipe)))
+                              utl-quelpa-recipes))))
+            (if (null recipe)
+                (message "Warning: No `%s' package in utl-quelpa-recipes."
+                         name)
+              (quelpa recipe)))))
     (with-temp-buffer
       (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
-      (eval-buffer)))
-  (if packages
-      (dolist (name packages)
-        (let ((recipe (car (cl-member-if
-                            (lambda (recipe)
-                              (eq name
-                                  (if (listp recipe) (car recipe) recipe)))
-                            utl-quelpa-recipes))))
-          (if (null recipe)
-              (message "Warning: No `%s' package in utl-quelpa-recipes."
-                       name)
-            (quelpa recipe))))
-    (dolist (recipe utl-quelpa-recipes)
-      (quelpa recipe))))
+      (eval-buffer)
+      (utl-quelpa-update-install t))))
 
 (provide 'utl-elpa)
 
