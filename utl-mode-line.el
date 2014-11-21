bc9ae34e4d9ee51365ff731186a00a5668a91ab4
Alex Kost <alezost@gmail.com>
2014-11-21 23:02:44 +0300

    mode-line: Slight cleanup.

--- utl-mode-line.el
+++ utl-mode-line.el
@@ -8,7 +8,8 @@
 
 ;;; Mode names
 
-;; Idea from <http://www.masteringemacs.org/articles/2012/09/10/hiding-replacing-modeline-strings/>
+;; Idea from
+;; <http://www.masteringemacs.org/articles/2012/09/10/hiding-replacing-modeline-strings/>.
 
 (defvar utl-mode-names-alist nil
   "Alist of mode names.
@@ -32,12 +33,14 @@ This function is intended to be used in `after-change-major-mode-hook'."
 Some major modes like to override
 `mode-line-buffer-identification'.  If you want to force a mode
 to use the default value, call this function like this:
+
   (utl-mode-line-default-buffer-identification 'Man-mode)
   (utl-mode-line-default-buffer-identification 'dired-mode)"
   (let ((hook (intern (concat (symbol-name mode) "-hook"))))
     (add-hook hook
-              (lambda () (setq mode-line-buffer-identification
-                               (default-value 'mode-line-buffer-identification))))))
+              (lambda ()
+                (setq mode-line-buffer-identification
+                      (default-value 'mode-line-buffer-identification))))))
 
 
 ;;; Mode line process
