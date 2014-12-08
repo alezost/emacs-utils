35ab75a4ce425fac27977d18421060c3ab4c1879
Alex Kost <alezost@gmail.com>
2014-12-08 15:29:30 +0300

    gnus: Improve functions for finding URLs in articles

--- utl-gnus.el
+++ utl-gnus.el
@@ -1,12 +1,12 @@
-;;; utl-gnus.el --- Additional functionality for Gnus
+;;; utl-gnus.el --- Additional functionality for Gnus   -*- lexical-binding: t -*-
 
 ;; Author: Alex Kost <alezost@gmail.com>
 ;; Created: 30 Jul 2013
 
 ;;; Code:
 
-(require 'gnus)
 (require 'gnus-sum)
+(require 'gnus-art)
 (require 'utl-misc)  ; for utl-xor
 
 (defun utl-gnus-buffer-names ()
@@ -75,66 +75,125 @@ Start Gnus if needed."
     (utl-gnus-save-win-config)))
 
 
-;;; Processing multimedia links with emms
+;;; Finding URLs in summary and article buffers
+
+(defvar utl-gnus-link-re "\\<link\\>"
+  "Regexp matching a link name.
+Used in `utl-gnus-summary-find-link-url'.")
 
 (defvar utl-gnus-mm-url-re "\\.mp3$"
   "Regexp for multimedia links.
-Used in `utl-gnus-summary-get-mm-url'.")
+Used in `utl-gnus-summary-find-mm-url'.")
 
-(defun utl-gnus-summary-emms-add-url ()
-  "Add the first multimedia link from gnus article to emms playlist."
-  (interactive)
-  (emms-add-url (utl-gnus-summary-get-mm-url)))
+(defun utl-widget-next ()
+  "Move point to the next field or button.
+After the last widget, move point to the end of buffer."
+  ;; The code is a rework of `widget-move'.
+  (let ((old (widget-tabable-at))
+        (move (if widget-use-overlay-change
+                  (lambda () (goto-char (next-overlay-change (point))))
+                (lambda () (forward-char 1)))))
+    (funcall move)
+    (while (let ((new (widget-tabable-at)))
+             (and (or (null new) (eq new old))
+                  (not (eobp))))
+      (funcall move))))
 
-(defun utl-gnus-summary-emms-play-url ()
-  "Play the first multimedia link from gnus article with emms."
-  (interactive)
-  (emms-play-url (utl-gnus-summary-get-mm-url)))
-
-(defun utl-gnus-summary-get-url (regexp &optional group)
-  "Return the first URL from the gnus article matching REGEXP.
-If GROUP is non-nil, it should be a number specifying a
-parenthesized expression from REGEXP that should be returned."
-  (let ((article (gnus-summary-article-number)))
-    (or article
-        (error "No article to select"))
-    (gnus-configure-windows 'article)
-    ;; Selected subject is different from the current article's subject.
-    (if (or (null gnus-current-article)
-	    (null gnus-article-current)
-	    (/= article (cdr gnus-article-current))
-	    (not (equal (car gnus-article-current) gnus-newsgroup-name)))
-        (gnus-summary-display-article article))
-    (let ((url (gnus-eval-in-buffer-window gnus-article-buffer
-                 (goto-char (point-min))
-                 (utl-widget-find-url regexp))))
-      (if (null group)
-          url
-        (string-match regexp url)
-        (match-string group url)))))
-
-(defun utl-gnus-summary-get-mm-url ()
-  "Return the first link to multimedia url from the gnus article.
-Matching url is defined by `utl-gnus-mm-url-re'."
-  (utl-gnus-summary-get-url utl-gnus-mm-url-re))
-
-(defun utl-widget-find-url (re)
-  "Return the first widget-url matching regexp RE from point.
+(defun utl-gnus-article-find-url (predicate)
+  "Return the first widget URL matching PREDICATE.
 Return nil if no matches found."
-  (if (eobp)
-      nil
-    (if widget-use-overlay-change
-        (goto-char (next-overlay-change (point)))
-      (forward-char 1))
-    (if (widget-tabable-at)
-        ;; Text property with URL depends on `mm-text-html-renderer'.
-        (let ((url (or (get-text-property (point) 'gnus-string)
-                       (get-text-property (point) 'shr-url))))
-          (if (and (stringp url)
-                   (string-match re url))
-              url
-            (utl-widget-find-url re)))
-      (utl-widget-find-url re))))
+  (save-excursion
+    (article-goto-body)
+    (backward-char)
+    (utl-gnus-article-find-url-1 predicate)))
+
+(defun utl-gnus-article-find-url-1 (predicate)
+  (utl-widget-next)
+  (unless (eobp)
+    (let* ((point (point))
+           ;; Text property with URL depends on `mm-text-html-renderer'.
+           (url (or (get-text-property point 'gnus-string)
+                    (get-text-property point 'shr-url))))
+      (if (and url (funcall predicate url))
+          url
+        (utl-gnus-article-find-url-1 predicate)))))
+
+(defun utl-gnus-article-find-url-by-re (regexp &optional group)
+  "Return the first widget URL matching REGEXP.
+If GROUP is non-nil, it should be a number specifying a
+parenthesized expression from REGEXP that should be returned.
+Return nil if no matches found."
+  (let ((url (utl-gnus-article-find-url
+              (lambda (url) (string-match-p regexp url)))))
+    (if (null group)
+        url
+      (string-match regexp url)
+      (match-string group url))))
+
+(defun utl-gnus-article-find-url-by-name (regexp)
+  "Return the first widget URL with widget name matching REGEXP.
+Return nil if no matches found."
+  (utl-gnus-article-find-url
+   (lambda (_) (looking-at regexp))))
+
+(defmacro utl-gnus-summary-eval-in-article (&rest body)
+  "Display an article buffer and evaluate BODY there."
+  ;; The code is taken from `gnus-summary-next-page'.
+  `(let ((article (gnus-summary-article-number)))
+     (or article
+         (error "No article to select"))
+     (gnus-configure-windows 'article)
+     ;; Selected subject is different from the current article's subject.
+     (if (or (null gnus-current-article)
+             (null gnus-article-current)
+             (/= article (cdr gnus-article-current))
+             (not (equal (car gnus-article-current) gnus-newsgroup-name)))
+         (gnus-summary-display-article article))
+     (gnus-eval-in-buffer-window gnus-article-buffer
+       ,@body)))
+
+(defun utl-gnus-summary-find-url-by-re (regexp &optional group)
+  "Return the first URL from the gnus article matching REGEXP.
+See `utl-gnus-article-find-url-by-re' for details."
+  (utl-gnus-summary-eval-in-article
+   (utl-gnus-article-find-url-by-re regexp group)))
+
+(defun utl-gnus-summary-find-url-by-name (regexp)
+  "Return the first URL from the gnus article with name matching REGEXP.
+See `utl-gnus-article-find-url-by-name' for details."
+  (utl-gnus-summary-eval-in-article
+   (utl-gnus-article-find-url-by-name regexp)))
+
+(defun utl-gnus-summary-find-link-url ()
+  "Return the first \"link\" URL from the gnus article.
+Matching url is defined by `utl-gnus-link-re'."
+  (utl-gnus-summary-find-url-by-name utl-gnus-link-re))
+
+(defun utl-gnus-summary-find-mm-url ()
+  "Return the first multimedia URL from the gnus article.
+Matching url is defined by `utl-gnus-mm-url-re'."
+  (utl-gnus-summary-find-url-by-re utl-gnus-mm-url-re))
+
+;;;###autoload
+(defun utl-gnus-summary-browse-link-url ()
+  "Browse the first \"link\" URL from the gnus article."
+  (interactive)
+  (browse-url (utl-gnus-summary-find-link-url)))
+
+(declare-function emms-add-url "emms-source-file" (url))
+(declare-function emms-play-url "emms-source-file" (url))
+
+;;;###autoload
+(defun utl-gnus-summary-emms-add-url ()
+  "Add the first multimedia URL from gnus article to EMMS playlist."
+  (interactive)
+  (emms-add-url (utl-gnus-summary-find-mm-url)))
+
+;;;###autoload
+(defun utl-gnus-summary-emms-play-url ()
+  "Play the first multimedia URL from gnus article with EMMS."
+  (interactive)
+  (emms-play-url (utl-gnus-summary-find-mm-url)))
 
 
 ;;; Convert Atom to RSS
