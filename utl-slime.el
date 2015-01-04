f213be7a6f3cf70bdac088d9a53ff9bdd3e253a1
Alex Kost <alezost@gmail.com>
2015-01-04 22:23:44 +0300

    slime: Add autoloads; silence compiler

--- utl-slime.el
+++ utl-slime.el
@@ -5,6 +5,9 @@
 
 ;;; Code:
 
+(require 'slime)
+
+;;;###autoload
 (defun utl-slime-eval-dwim ()
   "Eval (with slime) last sexp or region if it is active."
   (interactive)
@@ -12,6 +15,9 @@
       (slime-eval-region (region-beginning) (region-end))
     (slime-eval-last-expression)))
 
+(defvar slime-repl-input-start-mark)
+
+;;;###autoload
 (defun utl-slime-repl-kill-whole-line (arg)
   "Similar to `kill-whole-line', respecting slime-repl prompt."
   (interactive "p")
