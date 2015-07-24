9a18c36fa6c4d13f11b2843ade9c48be4cd5a48f
Alex Kost <alezost@gmail.com>
2015-07-24 22:11:42 +0300

    pcomplete: Remove unneeded code.
    With "pcmpl-args" package, these hacks are not needed anymore.

--- utl-pcomplete.el
+++ utl-pcomplete.el
@@ -5,26 +5,6 @@
 
 ;;; Code:
 
-(require 'pcomplete)
-
-(defvar utl-pcomplete-skipped-commands
-  '("sudo" "xargs")
-  "List of special commands for `utl-pcomplete-reduce-args-maybe'.")
-
-(defun utl-pcomplete-reduce-args-maybe (&rest _)
-  "Change some global variables to complete a special command properly.
-If a command from `utl-pcomplete-skipped-commands' is being
-completed, skip it and perform completion as if the next argument
-was the current command."
-  (when pcomplete-args
-    (let ((cmd (file-name-nondirectory (car pcomplete-args)))
-          (rest-args (cdr pcomplete-args)))
-      (when (and rest-args
-                 (member cmd utl-pcomplete-skipped-commands))
-        (setq pcomplete-args rest-args
-              pcomplete-last (1- pcomplete-last))
-        (utl-pcomplete-reduce-args-maybe)))))
-
 (defun utl-pcomplete-no-space ()
   "Do not terminate a completion with space in the current buffer."
   (setq-local pcomplete-termination-string ""))
