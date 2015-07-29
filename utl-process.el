80712a88127fd2b6c982a46360f22bca2130b355
Alex Kost <alezost@gmail.com>
2015-07-29 20:53:56 +0300

    process: Add "hooks system" for calling processes

--- utl-process.el
+++ utl-process.el
@@ -27,6 +27,48 @@ Interactively prompt for PROCESS name."
                        (mapcar #'process-name (process-list))))))
   (delete-process process))
 
+
+;; Hooks for starting/calling processes
+
+(defvar utl-before-process-functions '(utl-process-message)
+  "Functions to be called before Emacs starts an external process.
+Each function is called by applying to ARGS.  The first element
+of ARGS is a program name of the process, and the rest are
+program arguments.")
+
+(defun utl-process-message (&rest args)
+  "Display message about ARGS."
+  (message "Process to run: %S" args))
+
+(defun utl-run-before-process-hook (program args)
+  "Run `utl-before-process-functions' using PROGRAM and ARGS."
+  (apply #'run-hook-with-args
+         'utl-before-process-functions program args))
+
+(defun utl-run-before-call-process-hook
+    (program &optional infile destination display &rest args)
+  "Run `utl-before-process-functions' using PROGRAM and ARGS."
+  (utl-run-before-process-hook program args))
+
+(defun utl-run-before-start-process-hook
+    (name buffer program &rest args)
+  "Run `utl-before-process-functions' using PROGRAM and ARGS."
+  (utl-run-before-process-hook program args))
+
+;;;###autoload
+(defun utl-enable-process-hooks ()
+  "Make `utl-before-process-functions' active."
+  (interactive)
+  (advice-add 'call-process :before #'utl-run-before-call-process-hook)
+  (advice-add 'start-process :before #'utl-run-before-start-process-hook))
+
+;;;###autoload
+(defun utl-disable-process-hooks ()
+  "Make `utl-before-process-functions' inactive."
+  (interactive)
+  (advice-remove 'call-process #'utl-run-before-call-process-hook)
+  (advice-remove 'start-process #'utl-run-before-start-process-hook))
+
 (provide 'utl-process)
 
 ;;; utl-process.el ends here
